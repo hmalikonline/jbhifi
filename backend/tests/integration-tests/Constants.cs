f0a65baadb4bcf1bae821232dd018a7688d5672c
hmalik <hmalikonline@gmail.com>
2025-07-06 13:07:58 +1000

    adding another integration test to verify rate limit is correctly partitioned by api key

--- backend/tests/integration-tests/Constants.cs
+++ backend/tests/integration-tests/Constants.cs
@@ -1,6 +1,6 @@
 public static class TestConstants
 {
-    public static string ApiKeys = "e123456,876c43b";
+    public static string ApiKeys = "e123456,876c43b,109ff45,j8bs723";
     public static int RateLimit_MaxRequests = 1;
     public static int RateLimit_TimeWindowInSeconds = 4;
     
--- backend/tests/integration-tests/RateLimitingTests.cs
+++ backend/tests/integration-tests/RateLimitingTests.cs
@@ -46,7 +46,8 @@ public class RateLimitingTests : IClassFixture<CustomWebApplicationFactory>
 
         var apiKeyToUse = apiKeyList[0]; //use the first key in the list
 
-        //making 2 requests with a delay that's within acceptable rate limit
+        //making 2 requests
+
         //first request will be instant, subsequent requests will be delayed
         var responseStatus = await Call_API_After_A_Delay(0, apiKeyToUse);
         Assert.True(responseStatus == HttpStatusCode.OK);
@@ -57,23 +58,46 @@ public class RateLimitingTests : IClassFixture<CustomWebApplicationFactory>
     }
 
     [Fact]
-    public async Task Verify_Requests_Are_Blocked_Beyond_Rate_Limit()
+    public async Task Verify_Requests_Are_Blocked_When_Rate_Limit_Is_Exceeded()
     {
         var singleRequestInterval = (int)Math.Ceiling((double)TestConstants.RateLimit_TimeWindowInSeconds / TestConstants.RateLimit_MaxRequests);
 
-        var apiKeyToUse = apiKeyList[1]; //use the second key in the list
+        var apiKeyToUse = apiKeyList[1]; //use the second key in the list, so it doesn't interfere with other tests
 
-        //making 2 requests 
+        //making 2 requests
+
         //1st request will be instant, subsequent requests will be delayed
         var responseStatus = await Call_API_After_A_Delay(0, apiKeyToUse);
         Assert.True(responseStatus == HttpStatusCode.OK);
 
         //making 2nd request in half the acceptable interval, to cause the request rate go above rate limit
-        responseStatus = await Call_API_After_A_Delay((int)singleRequestInterval / 2, apiKeyToUse);
+        responseStatus = await Call_API_After_A_Delay((int)(singleRequestInterval / 2), apiKeyToUse);
         Assert.True(responseStatus == HttpStatusCode.TooManyRequests); //the second request should receive "too many requests" status
 
     }
 
+    /// <summary>
+    /// Making requests that will exceed rate limit, however because the requests are made with different api keys, they get processed
+    /// </summary>
+    /// <returns></returns>
+    [Fact]
+    public async Task Verify_Rate_Limit_Is_Paritioned_By_Keys()
+    {
+        var singleRequestInterval = (int)Math.Ceiling((double)TestConstants.RateLimit_TimeWindowInSeconds / TestConstants.RateLimit_MaxRequests);
 
+        var apiKeyToUse = apiKeyList[2]; //use a key in the list
+        
+        //making 2 requests
+
+        //first request will be instant, subsequent requests will be delayed
+        var responseStatus = await Call_API_After_A_Delay(0, apiKeyToUse);
+        Assert.True(responseStatus == HttpStatusCode.OK);
+
+        //making 2nd request in half the acceptable interval, to cause the request rate go above rate limit
+        //however, since 2nd request is made with a different api key, it goes through
+        apiKeyToUse = apiKeyList[3]; //use another key in the list
+        responseStatus = await Call_API_After_A_Delay((int)(singleRequestInterval / 2), apiKeyToUse);
+        Assert.True(responseStatus == HttpStatusCode.OK);
+    }
 
 }
