14910e79c5cfbc3164d9d7cb689c985e51bc4452
hmalik <hmalikonline@gmail.com>
2025-07-06 19:02:52 +1000

    building form handler server action code

--- frontend/weather-app/app/handlers/weather-handler.ts
+++ frontend/weather-app/app/handlers/weather-handler.ts
@@ -4,13 +4,37 @@ import { fetchWeather } from "@/lib/data-access"
 import { WeatherRequest } from "@/lib/dtos"
 import { Weather } from "@/lib/entities"
 
-export const getWeather = async (prevState: any, formData: FormData) => {
+
+interface OperationState {
+    success: boolean,
+    errors: string[]
+}
+
+export type GetWeatherFormState = OperationState & {    
+    weather: Weather|null
+}
+
+export const getWeather = async (prevState: any, formData: FormData) : Promise<GetWeatherFormState> => {
     //read and validate form data
     const weatherRequest:WeatherRequest = {
         city: formData.get("city") as string,
         country: formData.get("country") as string
     }
     
+    let errors: GetWeatherFormState['errors'] = [];
+    let weather:Weather|null = null;
+
     //fetch weather from data access layer
-    const weather:Weather|null = await fetchWeather(weatherRequest);
+    try{
+        weather = await fetchWeather(weatherRequest);
+    }
+    catch (ex) {
+        errors.push(ex instanceof Error ? ex.message : String(ex));        
+    }
+
+    return {
+        errors:errors,
+        success: true,
+        weather: weather
+    };
 }
--- frontend/weather-app/lib/dtos.ts
+++ frontend/weather-app/lib/dtos.ts
@@ -2,3 +2,4 @@ export type WeatherRequest = {
     city: string,
     country: string,
 }
+
