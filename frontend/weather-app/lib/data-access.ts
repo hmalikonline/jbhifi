6648ac4c1c862694e59a081b856885220e68d31c
hmalik <hmalikonline@gmail.com>
2025-07-06 18:37:21 +1000

    implementing api call logic and application configuration setup

--- frontend/weather-app/lib/data-access.ts
+++ frontend/weather-app/lib/data-access.ts
@@ -1,6 +1,33 @@
 import { WeatherRequest } from "./dtos";
 import { Weather } from "./entities";
 
-export const fetchWeather = async (request: WeatherRequest): Promise<Weather|null> => {
-    return null;
+ 
+export const fetchWeather = async (weatherRequest: WeatherRequest): Promise<Weather|null> => {
+        
+    const request: RequestInit = {            
+            cache: 'no-store',
+            method: 'GET',
+            headers: {
+                "x-api-key": `${process.env.API_KEY}`
+              }    
+        };
+
+    const apiEndPointUrl = `${process.env.API_BASE_URL}/weatherforecast?city=${weatherRequest.city}&country=${weatherRequest.country}`
+
+    try{
+        const response = await fetch(apiEndPointUrl, request);
+
+        if(response.ok)
+        {
+            var weather: Weather|null = await response.json();
+            return weather;            
+        }else{
+            console.log(response); //log failed response for alert/investigation
+            throw new Error("Couldn't fetch weather.");
+        }
+    }catch (ex){
+        console.log(ex); //log exception  for alert/investigation
+        throw new Error("Couldn't fetch weather.");        
+    }
+        
 }
